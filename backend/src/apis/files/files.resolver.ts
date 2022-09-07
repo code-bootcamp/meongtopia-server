f6d8a9e8588813e1be151ddfa82a1565257a54c3
1hana26 <seolhana6206@gmail.com>
2022-09-07 18:11:56 +0900

    :wrench: fix : fix uploadfile
    fix uploadfile

--- backend/src/apis/files/files.resolver.ts
+++ backend/src/apis/files/files.resolver.ts
@@ -16,4 +16,11 @@ export class FilesResolver {
     // console.log(files);
     return this.filesService.upload({ files });
   }
+  uploadOneFile(
+    @Args({ name: 'file', type: () => GraphQLUpload }) file: FileUpload, // graphql과 typescript 두개다 데이터 타입을 지정
+  ) {
+    //주소를 보내주기 때문에 Mutation의 return type이 string으로 지정.
+    // console.log(files);
+    return this.filesService.uploadOne({ file });
+  }
 }
--- backend/src/apis/files/files.service.ts
+++ backend/src/apis/files/files.service.ts
@@ -1,5 +1,7 @@
 import { Storage } from '@google-cloud/storage';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { getToday } from 'src/commons/utils/utils';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class FilesService {
@@ -16,9 +18,10 @@ export class FilesService {
       waitedFiles.map(
         (el) =>
           new Promise((resolve, reject) => {
+            const fname = `${getToday()}/${uuidv4()}/origin/${el.filename}`;
             el.createReadStream()
-              .pipe(storage.file(el.filename).createWriteStream())
-              .on('finish', () => resolve(`${bucket}/${el.filename}`))
+              .pipe(storage.file(fname).createWriteStream())
+              .on('finish', () => resolve(`${bucket}/${fname}`))
               .on('error', () => reject('false'));
           }),
       ),
@@ -27,5 +30,25 @@ export class FilesService {
       throw new HttpException('이미지 업로드 오류', HttpStatus.CONFLICT);
     }
     return results; //성공하면 url, 실패하면 "실패"
+  }
+
+  async uploadOne({ file }) {
+    const bucket = process.env.BUCKET;
+    const storage = new Storage({
+      projectId: process.env.GCP_PROJECT_ID,
+      keyFilename: process.env.GCP_PROJECT_KEY_FILENAME,
+    }).bucket(bucket);
+    const fname = `${getToday()}/${uuidv4()}/origin/${file.filename}`;
+    const fileName = await new Promise((resolve, reject) => {
+      file
+        .createReadStream()
+        .pipe(storage.file(fname).createWriteStream())
+        .on('finish', () => resolve(`${bucket}/${fname}`))
+        .on('error', () => reject('false'));
+    });
+    if (fileName === 'false') {
+      throw new HttpException('이미지 업로드 오류', HttpStatus.CONFLICT);
+    }
+    return fileName;
   }
 }
